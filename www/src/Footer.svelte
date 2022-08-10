99fd9a24f718ff35f80e44cfdc33b57bf64b14a2
vrde <agranzot@mailbox.org>
2022-08-10 16:20:55 +0200

    Fix readme, footer link

--- www/src/Footer.svelte
+++ www/src/Footer.svelte
@@ -7,8 +7,10 @@
     <p>
       <a href={linkToContract()}>Contract</a> ·
       <a href="https://github.com/deeecent/decentpoems/">Source code</a> ·
-      <a href="https://github.com/deeecent/decentpoems/README.md">About</a> ·
-      Made with &lt;3 for
+      <a href="https://github.com/deeecent/decentpoems/blob/main/README.md"
+        >About</a
+      >
+      · Made with &lt;3 for
       <a href="https://buidlit.devpost.com/">Polygon BUIDL IT Summer 2022</a>
     </p>
   </section>
